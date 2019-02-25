485711bcfa57c68ffc58d441387c23ce851c362a
OnyxPay <onyxpay7@gmail.com>
2019-02-25 21:29:55 +0200

    Fixed *.Native.Invoke error in smart-contracts

--- code/CodeGenerate_By_Ast.py
+++ code/CodeGenerate_By_Ast.py
@@ -1315,6 +1315,7 @@ class Visitor_Of_FunCodeGen(ast.NodeVisitor):
 
                 syscall_name = sys_name.replace(ONTOLOGY_SC_FRAMEWORK, '')
                 syscall_name = syscall_name.replace(ONTOLOGY_SC_FRAMEWORK_boa, '').encode('utf-8')
+                syscall_name = syscall_name.replace('Ontology', 'OnyxChain').encode('utf-8')
 
             length = len(syscall_name)
             systemcall_name_array = bytearray([length]) + bytearray(syscall_name)
